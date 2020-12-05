e7d3e0994f8ce67ecf2957c5a0510ac6b38a9fb3
znzl43313 <znzl43313@daum.net>
2020-12-06 01:46:57 +0900

    캡스톤 디자인 기말과제

---
+++ app/src/main/java/com/example/movie/MovieListResult.java
@@ -0,0 +1,13 @@
+package com.example.movie;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+    String yearWeekTime;
+
+    ArrayList<Movie> weeklyBoxOfficeList = new ArrayList<Movie>();
+
+}
